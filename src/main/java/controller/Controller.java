a15f4f2fa128a61b1392a320edc8d0383c39593f
pyriteC <89157444+pyriteC@users.noreply.github.com>
2023-03-26 17:48:03 -0600

    I've moved the file where it needs to be

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,24 @@
+package controller;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class Controller extends GameApplication {
+
+	public Controller() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void initSettings(GameSettings settings) {
+		// TODO Auto-generated method stub
+		settings.setTitle("this feels like a troll");
+	}
+waaa
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+}
